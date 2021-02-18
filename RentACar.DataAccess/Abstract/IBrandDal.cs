e6cf505d27a1575882f5ddc0e91cebdcea3ce022
Ugur Timurcin <utimurcin@outlook.com>
2021-02-18 18:38:13 +0300

    dal interface and class created for brand

---
+++ RentACar.DataAccess/Abstract/IBrandDal.cs
@@ -0,0 +1,14 @@
+﻿using RentACar.Core.DataAccess;
+using RentACar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.DataAccess.Abstract
+{
+    public interface IBrandDal : IEntityRepository<Brand>
+    {
+    }
+}
