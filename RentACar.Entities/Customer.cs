a0a8df0232d4d941bc85d74476b4ca703b0a074b
Ugur Timurcin <utimurcin@outlook.com>
2021-02-20 18:20:29 +0300

    Customer entity added

---
+++ RentACar.Entities/Customer.cs
@@ -0,0 +1,18 @@
+﻿using RentACar.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Entities
+{
+    public class Customer : IEntity
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+
+        public int AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+    }
+}
