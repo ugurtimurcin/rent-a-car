aeb1f2fc28cbdcc8160381dafec70996618985af
Ugur Timurcin <utimurcin@outlook.com>
2021-02-25 18:49:51 +0300

    CarImage entitiy created

---
+++ RentACar.Entities/CarImage.cs
@@ -0,0 +1,19 @@
+﻿using RentACar.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Entities
+{
+    public class CarImage : IEntity
+    {
+        public int Id { get; set; }
+        public string ImagePath { get; set; } = "default.jpg";
+        public DateTime DateTime { get; set; } = DateTime.Now;
+
+        public int CarId { get; set; }
+        public Car Car { get; set; }
+    }
+}
