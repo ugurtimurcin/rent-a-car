e37b3ace9f874157a3cdaa471d04bfdce90c4739
Ugur Timurcin <utimurcin@outlook.com>
2021-02-25 18:47:13 +0300

    carImage mapped

---
+++ RentACar.DataAccess/Concrete/Mapping/CarImageMap.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RentACar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.DataAccess.Concrete.Mapping
+{
+    public class CarImageMap : IEntityTypeConfiguration<CarImage>
+    {
+        public void Configure(EntityTypeBuilder<CarImage> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+
+            builder.HasOne(x => x.Car).WithMany(x => x.CarImages).HasForeignKey(x => x.CarId).OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
