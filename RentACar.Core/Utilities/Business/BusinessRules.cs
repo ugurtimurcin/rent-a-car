7e34a8d561ed6921e5cffa2ba438b0956e2d0976
Ugur Timurcin <utimurcin@outlook.com>
2021-02-25 18:45:11 +0300

    business rules created

---
+++ RentACar.Core/Utilities/Business/BusinessRules.cs
@@ -0,0 +1,25 @@
+﻿using RentACar.Core.Utilities.Results.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Core.Utilities.Business
+{
+    public class BusinessRules
+    {
+        public static IResult Run(params IResult[] logics)
+        {
+            foreach (var logic in logics)
+            {
+                if (!logic.Success)
+                {
+                    return logic;
+                }
+            }
+
+            return null;
+        }
+    }
+}
