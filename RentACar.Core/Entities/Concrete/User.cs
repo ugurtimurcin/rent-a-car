246947a76b0b4461a33783d7b59a41e49fed0df0
Ugur Timurcin <utimurcin@outlook.com>
2021-03-01 09:43:19 +0300

    AppUser table name changed to Table, columns also changed. Then moved to Core layer.

--- RentACar.Core/Entities/Concrete/User.cs
+++ RentACar.Core/Entities/Concrete/User.cs
@@ -1,19 +1,22 @@
-﻿using RentACar.Core.Entities;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RentACar.Entities
+namespace RentACar.Core.Entities.Concrete
 {
-    public class AppUser : IEntity
+    public class User : IEntity
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string UserName { get; set; }
-        public string Password { get; set; }
         public string Email { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public bool State { get; set; }
+
+
+
     }
 }
