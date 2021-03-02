d668cf81a0a081cf9cd3b6168939d5e5e227ffb4
Ugur Timurcin <utimurcin@outlook.com>
2021-03-02 12:32:59 +0300

    New error messages added.

--- RentACar.Business/Constants/Messages.cs
+++ RentACar.Business/Constants/Messages.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RentACar.Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,5 +13,10 @@ namespace RentACar.Business.Constants
         public static string Removed = "Removed!";
         public static string Updated = "Updated!";
         public static string CarHasMoreThan5ImagesError = "There should not be more than 5 images of the car";
+        public static string UserNotExists = "User not exists";
+        public static string PasswordError = "Wrong password";
+        public static string UserExist = "User already exists";
+        public static string UserRegistered = "User succesfully registered";
+        public static string TokenCreated = "Access token created";
     }
 }
