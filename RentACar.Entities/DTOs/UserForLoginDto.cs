86756537996f6dce10a6a39b78e9c9c5f79f9c3e
Ugur Timurcin <utimurcin@outlook.com>
2021-03-02 12:38:28 +0300

    For registering and logging processing dto created.

---
+++ RentACar.Entities/DTOs/UserForLoginDto.cs
@@ -0,0 +1,15 @@
+﻿using RentACar.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Entities.DTOs
+{
+    public class UserForLoginDto : IDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
